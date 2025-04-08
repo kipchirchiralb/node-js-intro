cc025c86475fea728e66a46661a0a6cfabb6e4ed
Albert Kipchirchir <albertkipchirchir02@gmail.com>
2025-04-08 10:14:25 +0300

    node js intro

---
+++ index.js
@@ -0,0 +1,8 @@
+const fsystem = require('fs');
+
+fsystem.mkdirSync("images")
+fsystem.mkdirSync("botos")
+fsystem.mkdirSync("scripts")
+
+fsystem.writeFileSync("notes.txt", "This is a note file created by node.js")
+fsystem.writeFileSync("images/icon.svg", `<svg fill="#000000" width="800px" height="800px" viewBox="0 0 24 24" id="check" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color"><path id="primary" d="M10,18a1,1,0,0,1-.71-.29l-5-5a1,1,0,0,1,1.42-1.42L10,15.59l8.29-8.3a1,1,0,1,1,1.42,1.42l-9,9A1,1,0,0,1,10,18Z" style="fill: rgb(0, 0, 0);"></path></svg>`)
